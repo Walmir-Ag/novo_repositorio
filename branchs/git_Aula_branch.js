6e113a979c67c62a9a2a8f5d209e579922acf9eb
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-12 17:39:07 -0300

    erro de escrita

--- branchs/git_Aula_branch.js
+++ branchs/git_Aula_branch.js
@@ -1,11 +1,11 @@
 /*  
     git checkout:
-    TAMBEM deleta todas as mudanças locais 
+    TAMBEM sobescreve todas as mudanças locais 
     trocar de branchs
-    
+
     git checkout -b : criar branchs
     
-
+    hash: é o codigo criptografado(identificador),  assim podendo navegar nos commits para voltar caso queira
 
 
 */
